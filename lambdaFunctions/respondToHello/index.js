3ce4e57defdcbf9915ba34159f8074ec89f3319d
Nikola Nisic <niknisic@gmail.com>
2020-06-17 14:06:09 -0400

    Working signature generation & comparison. Work into error handling next

--- lambdaFunctions/respondToHello/index.js
+++ lambdaFunctions/respondToHello/index.js
@@ -1,5 +1,14 @@
+var Base64 = require('js-base64').Base64;
+const crypto = require('crypto');
 
 exports.handler = async (event) => {
+
+    const requestTimestamp = event.headers['x-slack-request-timestamp'];
+    const requestBody = event.body;
+    const slackSignature = event.headers['x-slack-signature'];
+
+    console.log('RESULT OF SIGNATURE COMPARISON: ');
+    console.log(compareSignatures(requestTimestamp, requestBody, slackSignature));
 
     /*
     * Build message payload to send back in response body as a response to Slack's slash command POST request.
@@ -40,3 +49,17 @@ exports.handler = async (event) => {
     };
     return response;
 };
+
+
+function compareSignatures(timestamp, requestBody, slackSignature) {
+  const signingSecret = process.env.signing_secret;
+  const decodedBody = Base64.decode(requestBody);
+  const baseString = 'v0:' + timestamp + ':' + decodedBody;
+  const currentTime = Date.now()/1000;
+  const hmac = crypto.createHmac('sha256', signingSecret);
+  const undigestedSignature = hmac.update(baseString);
+  const mySignature = 'v0=' + hmac.digest('hex');
+
+  return (mySignature.localeCompare(slackSignature) === 0 && currentTime - timestamp < 60*2);
+
+}

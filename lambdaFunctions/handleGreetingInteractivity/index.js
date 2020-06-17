51d5d57972ae26dbd9c917e057f857c3c319cb41
Nikola Nisic <niknisic@gmail.com>
2020-06-16 22:26:44 -0400

    Full bot functionality is complete. Security and error handling next

--- lambdaFunctions/handleGreetingInteractivity/index.js
+++ lambdaFunctions/handleGreetingInteractivity/index.js
@@ -4,17 +4,38 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
 
-  //Decode Slack payload from Base64 encoded URI to JSON
-  const eventBodyDecoded = Base64.decode(event.body);
+  /*
+  * Decode Slack payload from Base64 encoded URI to JSON
+  */
   const payloadJson = JSON.parse(decodeURIComponent(Base64.decode(event.body).split("=")[1]));
-  const interactionType = payloadJson.type;
-  const triggerId = payloadJson.trigger_id;
+
+  /*
+  * Get Slack OAuth token from environment variable (configured in AWS Lambda console) so that bot can call Slack API methods.
+  */
   const slackToken = process.env.slack_token;
+
+  /*
+  * Get the interaction type from the payload to know where to go next with it.
+  */
+  const interactionType = payloadJson.type;
 
   if(interactionType === "block_actions") {
 
+    /*
+    * Get channel ID from block_actions payload to put in private_metadata field of the view_submission payload (which is sent after user submits modal).
+    * I do this because Slack's default view_submission payload doesn't pass any information about the current channel, but it is needed
+    * to post a message back into the same channel.
+    */
     const channelId = payloadJson.channel.id;
 
+    /*
+    * Get triggerID from block_actions payload so that the bot can open a modal in response.
+    */
+    const triggerId = payloadJson.trigger_id;
+
+    /*
+    * Build view payload with trigger ID,
+    */
     const viewPayload =
       {
         "trigger_id": triggerId,
@@ -22,7 +43,7 @@ exports.handler = async (event) => {
         	"type": "modal",
         	"title": {
         		"type": "plain_text",
-        		"text": "Greeting bot",
+        		"text": "Greeting Bot",
         		"emoji": true
         	},
         	"submit": {
@@ -52,6 +73,9 @@ exports.handler = async (event) => {
         }
       };
 
+    /*
+    * Use node-fetch library to send POST request to views.open API to push modal view onto view stack.
+    */
     await fetch('https://slack.com/api/views.open', {
       method: "post",
       body: JSON.stringify(viewPayload),
@@ -61,47 +85,69 @@ exports.handler = async (event) => {
       }
     });
 
+    /*
+    * Return response with HTTP status code 200 back to Slack.
+    */
     const response = {
+      statusCode: 200,
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*"
       },
       "isBase64Encoded": false,
-        statusCode: 200,
-        body: JSON.stringify('It worked!'),
     };
 
     return response;
 
   } else if(interactionType === "view_submission") {
 
+    /*
+    * Get channel ID from the value of the private_metadata field that I put in the view_submission payload (submitting the name modal).
+    * I put this in private_metadata because, as far as I can tell, the view_submission payload provides no channel information.
+    */
     const channelId = payloadJson.view.private_metadata;
+
+    /*
+    * Get block ID and action ID from the view's blocks array so that I can use them to access the user's input value from view's state object.
+    * Use the input value to personalize the text in the message payload.
+    */
     const blockId = payloadJson.view.blocks[0].block_id;
     const actionId = payloadJson.view.blocks[0].element.action_id;
     const inputValue = payloadJson.view.state.values[blockId][actionId].value;
+    const greetingText = "Hello " + inputValue + "!";
 
-    const viewPayload = {
+    /*
+    * Build message payload with channel ID, custom username, and personalized text to send in body of chat.postMessage POST request.
+    */
+    const messagePayload = {
       "channel": channelId,
+      "username": "Greeting Bot",
     	"blocks": [
     		{
     			"type": "section",
     			"text": {
     				"type": "plain_text",
-    				"text": "Hello " + inputValue + "!"
+    				"text": greetingText
     			}
     		}
-    	]
+    	],
     }
 
+    /*
+    * Use node-fetch library to send POST request to chat.postMessage API to post personalized greeting in same channel.
+    */
     await fetch('https://slack.com/api/chat.postMessage', {
       method: "post",
-      body: JSON.stringify(viewPayload),
+      body: JSON.stringify(messagePayload),
       headers: {
       "Content-Type": "application/json",
       "Authorization": slackToken
       }
     });
 
+    /*
+    * Return response with HTTP status code 200 back to Slack.
+    */
     const response = {
         statusCode: 200,
         headers: {
--- lambdaFunctions/respondToHello/index.js
+++ lambdaFunctions/respondToHello/index.js
@@ -1,13 +1,10 @@
-var Base64 = require('js-base64').Base64;
-const queryString = require('query-string');
-
 
 exports.handler = async (event) => {
 
-    //Decode Slack payload from Base64 encoded URI to JSON
-    const eventBodyJson = queryString.parse(Base64.decode(event.body));
-
-    const viewPayload = {
+    /*
+    * Build message payload to send back in response body as a response to Slack's slash command POST request.
+    */
+    const messagePayload = {
       "blocks": [
         {
           "type": "actions",
@@ -27,6 +24,11 @@ exports.handler = async (event) => {
       ]
     }
 
+    /*
+    * Return response with HTTP status code 200 back to Slack.
+    * Also put message payload in response body so that the bot can post a message in response to the slash command.
+    * This is the bot's entry point and the beginning of the flow of use.
+    */
     const response = {
         statusCode: 200,
         headers: {
@@ -34,7 +36,7 @@ exports.handler = async (event) => {
           "Access-Control-Allow-Origin": "*"
         },
         "isBase64Encoded": false,
-        body: JSON.stringify(viewPayload),
+        body: JSON.stringify(messagePayload),
     };
     return response;
 };
